e236761af3ffb297320497ebfa9e934c1c7b1d38
Tylere Couture <tylerecouture@gmail.com>
2019-07-06 09:58:58 -0700

    linted

--- app/core/models.py
+++ app/core/models.py
@@ -5,19 +5,20 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        user=self.model(email=email, **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
     def create_superuser(self, email, password):
-        user=self.create_user(email, password)
+        user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
-        
+
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
 
